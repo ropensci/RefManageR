93ba7c0d8c29eb76428e6a69877ffbfb14f2fe17
Mathew W. McLean <mmclean@stat.tamu.edu>
2013-11-22 09:11:43 -0600

    ReadPDFs seems to be working

--- code/RefManageR/R/RoughReadPDFtoText.R
+++ code/RefManageR/R/RoughReadPDFtoText.R
@@ -41,13 +41,6 @@ docpm2 <- readLines('pubmed2.txt', encoding = 'UTF-8')
 jstordoc <- readLines('Gelfand-ImproperPriorsForGLMs(JASA1999).txt', encoding = 'UTF-8')
 jstordoc2 <- readLines('jstorLT.txt', encoding = 'UTF-8')
 
-# keywords
-ind <- grep('K[Ee][Yy][[:space:]]?[Ww][Oo][Rr][Dd][Ss]:[[:space:]]*', doc)
-m <- regexpr('Keywords:[[:space:]]*[A-Za-z]+\\s', doc, perl=TRUE)
-regmatches(doc, m)
-keywords <- sub('Keywords:[[:space:]]*', '', doc[ind]) 
-keywords <- gsub(';', ',', keywords)
-
 tnames <- c('Ana-Maria Staicu', 'A.-M. Staicu', 'R. de Vries', 'Oscar de la Hoya', 'Mathew McLean', 
             'Manuel Febrero-Bande', 'Shaq O\'Neal', 'T. T. T. Smith')
 # author     first name                 opt. middle name          last name
@@ -82,32 +75,7 @@ gsub("[^[:alpha:] '-]", '', tempnames)  # remove special characters end of names
 # ind <- regexpr('[[:upper:]](\\.|[a-z]+)[.]?[[:space:]][A-Z]?[a-z]*[.]?[[:space:]]?[[:upper:]][[:alpha:]]+', docme)
 # regmatches(docme, ind)
 
-GetAuthorTitle <- function(doc){
-  ind <- grep('^A[Bb][Ss][Tt][Rr][Aa][Cc][Tt][:.]?\\>', doc)[1]
-  if (!is.na(ind) && ind > 2)  # assume title/author comes before Abstract. need 2nd cond. for ind==1
-    doc <- doc[1:(ind-1)]
-#  browser()
-#   aut.ind <- regexpr(paste0("^([A-Z][a-z]*[\\.]?[ -]",  # first name, maybe hypenated or abbrev.
-#                       "([A-Z][a-z]*[\\.]?[ -])*",  # optional middle name or initial, maybe hypenated
-#                       "[[:upper:]][[:alpha:]'-]+.?[[:space:]]?",  # last name + potential extra char to 
-#                       "(, )?(Jr)?(II)?(III)?(IV)?(, )?(MD)?(, )?(Ph|HD)?.?",  # optional qualifications      
-#                       "(and)?[,;&$]?[[:space:]]?)+$"),  # and, ",", ";", or "&" to seperate names. Repeat
-#                doc)
-  
-  aut.ind <- regexpr(paste0(# invalid words negate match
-    "(?!Online|Supplement|Data|University|College|Institute|School)",
-    "^([A-Z][a-z]*[\\.]?[ -]",  # first name, maybe hypenated or abbrev.
-    "([A-Z][a-z]*[\\.]?[ -])*",  # optional middle name or initial, maybe hypenated
-    "[[:upper:]][[:alpha:]'-]+.?[[:space:]]?",  # last name + potential extra char to 
-    "(, )?(Jr)?(II)?(III)?(IV)?(, )?(MD)?(, )?(Ph|HD)?.?",  # optional qualifications      
-    "(?<!Online|Supplement|Data|University|College|Institute|School)", 
-    "(and)?[,;&$]?[[:space:]]?)+$"),  # and, ",", ";", or "&" to seperate names. Repeat
-                     doc, perl=TRUE)
-  aut.match <- regmatches(doc, aut.ind)
-  aut.match <- gsub(",?( MD)?,?( P(H|h)D)?", '', aut.match)  # remove MD and PhD
-  aut.match <- gsub("[^[:alpha:] ,'-](, MD)?(, P(H|h)D)?", '', aut.match)  # remove punct at end of last name
-  return(list(ind=which(aut.ind != -1), match=aut.match, ab.ind=ind))
-}
+
 
 
 aut.ind <- regexpr(paste0(# invalid words negate match
@@ -121,28 +89,7 @@ aut.ind <- regexpr(paste0(# invalid words negate match
                    doc, perl=TRUE)
 regmatches(doc, aut.ind)
 
-# volume
-#volume <- grep('(?:Vol|Volume)[[:punct:]]?[[:space:]]?[0-9]+', doc2, value=TRUE)[1]
-volume <- grep('(Vol|Volume)[[:punct:]]?[[:space:]]?[0-9]+', doc2, value=TRUE)[1]
-if (!is.na(volume)){
-  volume <- gsub('(Vol|Volume)[[:punct:]]?[[:space:]]?', '', volume)
-}else{
-  volume <- NULL
-}
 
-# number
-number <- grep('(No|Number)[[:punct:]]?[[:space:]]?[0-9]+', doc2, value=TRUE)[1]
-if (!is.na(number)){
-  number <- gsub('(No|Number)[[:punct:]]?[[:space:]]?', '', number)
-}else{
-  number <- NULL
-}
-
-# year
-# year <- grep('[(]?(19|20)[0-9]{2}[)]?', doc3, value=TRUE)[1]
-m <- regexpr('\\<(19|20)[0-9]{2}\\>', doc)
-if(any(m != -1))
-  year <- regmatches(doc, m)[1]
 # if(is.na(year)){
 #   year <- NULL
 # }else{
@@ -157,33 +104,3 @@ if(any(m != -1))
 
 # journal
 
-# arXiv
-ind <- grep('(arXiv)', doc)[1]
-if(!is.na(ind)){
-  arxinfo <- doc[ind]
-  eprinttype <- 'arXiv'
-  # need to check date since arXiv identifier format changed in Apr-07
-  m <- regexpr('[0-9]{1,2}[[:space:]][A-Z][a-z]{2}[[:space:]][0-9]{4}', arxinfo)
-  adate <- strptime(regmatches(arxinfo, m), format='%d %b %Y')
-  if (adate >= strptime('01 Apr 2007', format='%d %b %Y')){
-    m <- regexec('arXiv:([0-9]{4}[\\.][0-9]{4}v[0-9])[[:space:]]\\[([[:graph:]]+)\\]', arxinfo)
-    regm <- regmatches(arxinfo, m)
-    eprintclass <- regm[[1]][3]
-    eprint <- regm[[1]][2]
-  }else{
-    m <- regexec('arXiv:([[:graph:]]+)\\s', arxinfo)
-    regm <- regmatches(arxinfo, m)
-    eprint <- regm[[1]][2]
-  }
-  url <- paste0('http://arxiv.org/abs/', eprint)
-}
-
-# JSTOR
-ind <- grep('http://www\\.jstor\\.org/stable/([0-9]+)', jstordoc)[1]
-if (!is.na(ind)){
-  m <- ('http://www\\.jstor\\.org/stable/([0-9]+)', jstordoc[ind])
-}
-
-
-year <- gsub()
-year <- grep('')
