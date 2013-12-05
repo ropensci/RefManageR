025d47322ac315f897ce188945a09cfee9be050f
Mathew W. McLean <mmclean@stat.tamu.edu>
2013-12-05 14:10:10 -0600

    as.data.frame.BibEntry and as.BibEntry working

--- code/RefManageR/R/asBibEntry.R
+++ code/RefManageR/R/asBibEntry.R
@@ -1,12 +1,38 @@
 as.BibEntry <- function(x){
   if (is.character(x)){
     
+    if (is.na(x['bibtype']) || is.na(x['key']))
+      stop("Object of class character must have entries named bibtype and key.")
+    x <- as.list(x)
+    if (!is.null(x$author))
+      x$author <- as.person(x$author)
+    if (!is.null(x$editor))
+      x$editor <- as.person(x$editor)
+    attr(x, 'entry') <- x$bibtype
+    attr(x, 'key') <- x$key
+    x$bibtype <- NULL
+    x$key <- NULL
+    x <- MakeBibEntry(x, FALSE)
+  }else if(is.data.frame(x)){
+    .fields <- colnames(x)
+    if (is.null(x$bibtype))
+      stop("data.frame must have column for 'bibtype'.")
+    keys <- rownames(x)
+    if (keys[1] == '1')
+      warning('rownames of data.frame not meaningful for creating keys')
+
+    y <- vector('list', length(x))
+    for (i in seq_along(x)){
+      na.ind <- which(!is.na(x[i, ]))
+      y[[i]] <- as.BibEntry(c(setNames(as.character(x[i, na.ind]), .fields[na.ind]), key = keys[1]) )
+    }
+    y <- MakeCitationList(y)
+    return(y)
+
   }else if(is.list(x)){
     x <- relist.BibEntry(x)
-  }else if(is.data.frame(x)){
-    
   }else{
-    stop("Cannot coerce '", match.call()$x, "' to BibEntry")
+    stop(paste0("Cannot coerce object of class '", class(x), "' to BibEntry"))
   }
   return(x)
 }
