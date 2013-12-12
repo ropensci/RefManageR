0e152fa5c6d8a620c96d1b216316f3b6c879c0f0
Mathew W. McLean <mmclean@stat.tamu.edu>
2013-12-12 01:09:39 -0600

    started using Roxygen2; added head and tail; updated
    temporarily created .../R/TEMP-ROXY-DONE/ to indicate functions that
    have been converted to Roxygen2

--- code/RefManageR/R/open.BibEntry.R
+++ code/RefManageR/R/open.BibEntry.R
@@ -1,5 +1,27 @@
-open.BibEntry <- function(dtb, entry = 1, open.field = c('file', 'url', 'eprint', 'doi'), viewer){
-  bib <- dtb[[entry]]
+#' Open BibEntry in PDF viewer or web browser.
+#' 
+#' @param object of class BibEntry.
+#' @param entry numeric index or character key of entry in \code{bib} to open.
+#' @param open.field character vector of fields to use in \code{bib} to open the BibEntry.  
+#'   Possible fields are any combination of \dQuote{file},\dQuote{url}, \dQuote{eprint}, or \dQuote{doi}.  
+#'   \dQuote{eprint} is implemented for \code{eprinttype=} \dQuote{JSTOR}, \dQuote{PubMed}, or \dQuote{arXiv}.  
+#'   When multiple fields are specified, they are tried in the order they appear in the vector.
+#' @param viewer character string giving the name of the program to be used as hypertext browser. 
+#'   It should be in the PATH, or a full path specified. Alternatively, an R function to be called to invoke 
+#'   the browser.  Defaults to \code{getOptions(\dQuote{pdfviewer})} if \code{open.field = \dQuote{file}} and 
+#'   \code{getOptions(\dQuote{browser})}, otherwise.
+#' @keywords connection utilities  
+#' @seealso \code{\link{browseURL}}
+#' @method open BibEntry
+#' @examples 
+#' \dontrun{
+#' testbib <- ReadBib(system.file("REFERENCES.bib", package="bibtex"))
+#' open(testbib)
+#' testbib$file <- file.path(paste0(R.home(), '/doc/manual/R-intro.pdf'))
+#' open(testbib)
+#' }
+open.BibEntry <- function(bib, entry = 1, open.field = c('file', 'url', 'eprint', 'doi'), viewer){
+  bib <- bib[[entry]]
   stopifnot(length(dtb[[entry]]) == 1)
   bib.fields <- unlist(fields(bib))
   opened <- FALSE
