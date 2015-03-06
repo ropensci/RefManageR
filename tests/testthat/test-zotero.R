f10a3ab3c5a113e9f533da819189abdcc97a358a
Mathew W. McLean <mathew.w.mclean@gmail.com>
2015-03-06 08:47:15 -0600

    dont test if cant connect to zotero

--- tests/testthat/test-zotero.R
+++ tests/testthat/test-zotero.R
@@ -10,16 +10,22 @@ expect_length <- function(expr, len){
 
 test_that("basic Zotero search", {
    ## if (!RCurl::url.exists("http://api.zotero.org/users"))
-   ##     skip("Couldn't connect to Zotero")
-   expect_equal(length(ReadZotero(user='1648676', .params=list(q='bayesian', key='7lhgvcwVq60CDi7E68FyE3br',
-                                                      limit=2))), 2L)
+    ##     skip("Couldn't connect to Zotero")
+   res <- ReadZotero(user='1648676', .params=list(q='bayesian', key='7lhgvcwVq60CDi7E68FyE3br',
+                                         limit=2))
+   if (!length(res))
+       skip("Couldn't connect to Zotero")
+   expect_length(res, 2L)
 })
 
 test_that("Search specific collection", {
    ## if (!RCurl::url.exists("http://api.zotero.org/users"))
-   ##   skip("Couldn't connect to Zotero")
-   expect_equal(length(ReadZotero(user='1648676', .params=list(q='yu', key='7lhgvcwVq60CDi7E68FyE3br',
-                                                      collection='3STEQRNU'))), 3L)
+    ##   skip("Couldn't connect to Zotero")
+   res <- ReadZotero(user='1648676', .params=list(q='yu', key='7lhgvcwVq60CDi7E68FyE3br',
+                                         collection='3STEQRNU'))
+   if (!length(res))
+       skip("Couldn't connect to Zotero")
+   expect_length(res, 3L)
 })
 
 test_that("Search by tag", {
@@ -27,9 +33,14 @@ test_that("Search by tag", {
    ## if (!RCurl::url.exists("http://api.zotero.org/users"))
    ##     skip("Couldn't connect to Zotero")
    BibOptions(check.entries = "error")
-   expect_length(suppressMessages(ReadZotero(user='1648676', .params=list(key='7lhgvcwVq60CDi7E68FyE3br',
-      tag='Statistics - Machine Learning'))), 0L)
+   res <- suppressMessages(ReadZotero(user='1648676', .params=list(key='7lhgvcwVq60CDi7E68FyE3br',
+                                                          tag='Statistics - Machine Learning')))
+   expect_length(res, 0L)
    BibOptions(check.entries = FALSE)
-   expect_length(suppressMessages(ReadZotero(user='1648676',
-                  .params=list(key='7lhgvcwVq60CDi7E68FyE3br', tag='Statistics - Machine Learning'))), 10L)
+   res <- suppressMessages(ReadZotero(user='1648676',
+                                      .params=list(key='7lhgvcwVq60CDi7E68FyE3br',
+                                          tag='Statistics - Machine Learning')))
+   if (!length(res))
+       skip("Couldn't connect to Zotero")
+   expect_length(res, 10L)
 })
