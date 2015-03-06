c72f5b2c0ac50d0d2596467fec2b8650f79ce903
Mathew W. McLean <mathew.w.mclean@gmail.com>
2015-03-06 04:44:39 -0600

    fix testing of ReadBib

--- inst/tests/testthat/test-Read.R
+++ inst/tests/testthat/test-Read.R
@@ -1,14 +1,16 @@
 context("ReadBib")
-library(RefManageR)
 
-test_that("Read.Bib reads in 494 entries from RJC.bib", {
+## unloadNamespace("RefManageR")
+## library(RefManageR)
+
+test_that("ReadBib reads in 494 entries from RJC.bib", {
   file.name <- system.file("Bib", "RJC.bib", package="RefManageR")
   bib <- ReadBib(file.name)
   expect_equal(length(bib), 494L)
 })
 
-test_that("Read.Bib Ignores entry but does not stop with invalid author/editor", {
+test_that("ReadBib Ignores entry but does not stop with invalid author/editor", {
   f <- file.path(system.file("Bib", "badFormat.bib", package = "bibtex"))
-  bib <- ReadBib(f)
+  bib <- ReadBib(f, check = "error")
   expect_true(length(bib) == 1L)
 })
