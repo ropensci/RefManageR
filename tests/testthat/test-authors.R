617f69719ebaf18609c96a377df3c298f922c972
Mathew W. McLean <mathew.w.mclean@gmail.com>
2015-03-06 07:57:48 -0600

    fix author parsing tests for different locales

--- tests/testthat/test-authors.R
+++ tests/testthat/test-authors.R
@@ -1,58 +1,61 @@
 context("Author Parsing")
-library(RefManageR)
+library(bibtex)
 
 test_that("Smith, Jr., John", {
   authors <- "Smith, Jr., John"
-  parsed <- RefManageR:::ArrangeAuthors(authors)
+  parsed <- bibtex:::ArrangeAuthors(authors)
   expect_equal(length(parsed$family), 2L)
 })
 
 test_that("Smith, Jr., John and Mary {Tyler Moore}", {
   authors <- "Smith, Jr., John and Mary {Tyler Moore}"
-  parsed <- RefManageR:::ArrangeAuthors(authors)
+  parsed <- bibtex:::ArrangeAuthors(authors)
   expect_match(parsed$family[2], "Tyler Moore")
 })
 
 test_that("Smith, Jr., John and {MATLAB, Inc.}", {
   authors <- "Smith, Jr., John and {MATLAB, Inc.}"
-  parsed <- RefManageR:::ArrangeAuthors(authors)
+  parsed <- bibtex:::ArrangeAuthors(authors)
   expect_match(parsed$family[2], "MATLAB, Inc.")
   expect_equal(length(unlist(parsed$family[1])), 2L)
 })
 
 test_that("Smith, John Paul and {MATLAB, Inc.}", {
   authors <- "Smith, John Paul and {MATLAB, Inc.}"
-  parsed <- RefManageR:::ArrangeAuthors(authors)
+  parsed <- bibtex:::ArrangeAuthors(authors)
   expect_equal(length(unlist(parsed$given[1])), 2L)
 })
 
 test_that("{de Gama}, Vasco", {
   authors <- "{de Gama}, Vasco"
-  parsed <- RefManageR:::ArrangeSingleAuthor(authors)
+  parsed <- bibtex:::ArrangeSingleAuthor(authors)
   expect_equal(length(parsed$family), 2L)
 })
 
 test_that("Mark von Bommel", {
   authors <- "Mark von Bommel"
-  parsed <- RefManageR:::ArrangeSingleAuthor(authors)
+  parsed <- bibtex:::ArrangeSingleAuthor(authors)
   expect_equal(length(parsed$family), 2L)
 })
 
 test_that("de la Soul, Posdnous", {
   authors <- "de la Soul, Posdnous"
-  parsed <- RefManageR:::ArrangeSingleAuthor(authors)
+  parsed <- bibtex:::ArrangeSingleAuthor(authors)
   expect_equal(length(parsed$family), 2L)
 })
 
 test_that("des White, Jr., Walter", {
   authors <- "des White, Jr., Walter"
-  parsed <- RefManageR:::ArrangeSingleAuthor(authors)
+  parsed <- bibtex:::ArrangeSingleAuthor(authors)
   expect_equal(length(parsed$family), 3L)
 })
 
 test_that("{Herm{\\`e}s International S.A.} and Katzfu{\\ss}, Matthias", {
   authors <- "{Herm{\\`e}s International S.A.} and Katzfu{\\ss}, Matthias"
-  parsed <- RefManageR:::ArrangeAuthors(authors)
-  expect_match(parsed$family[[1]], "Herm�s International S.A.")
-  expect_match(parsed$family[[2]], "Katzfu�")
+  parsed <- bibtex:::ArrangeAuthors(authors)
+  ## Be careful about locales
+  ## expect_match(parsed$family[[1]], "Hermès International S.A.")
+  expect_true(grepl(parsed$family[[1]], "Hermès International S.A.", useBytes = TRUE))
+  ## expect_match(parsed$family[[2]], "Katzfuß")
+  expect_true(grepl(parsed$family[[2]], "Katzfuß", useBytes = TRUE))
 })
