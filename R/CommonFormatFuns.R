fa5b895e243c1a6a0798cee81f0543d10b8bed21
Mathew W. McLean <mathew.w.mclean@gmail.com>
2019-04-02 16:37:34 +1100

    Fix b6a7b3e if NULL/NA string; #62

--- R/CommonFormatFuns.R
+++ R/CommonFormatFuns.R
@@ -398,7 +398,7 @@ GetFormatFunctions <- function(docstyle = "text", DateFormatter){
     addPeriod <- function(string){
       ## #62: sub can cause conversion of unicode char to latin1 on windows
       ## sub("([^.?!])$", "\\1.", string, useBytes = TRUE)
-      if (grepl("([^.?!])$", string, useBytes = TRUE))
+      if (!.is_not_nonempty_text(string) && grepl("([^.?!])$", string, useBytes = TRUE))
         paste0(string, ".")
       else
         string
