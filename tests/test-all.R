5bf6a52b4885cf9ecb4a0285a26db51f3856c82a
Mathew W. McLean <mathew.w.mclean@gmail.com>
2020-10-27 21:17:55 +1100

    Fix code for testthat tests on/off CRAN; fix d5c5e2

--- tests/test-all.R
+++ tests/test-all.R
@@ -3,7 +3,7 @@ Sys.setenv(NOT_CRAN = "true")
 ## filter_patt <- "^(?!readPDF)[A-Za-z0-9]+$"
 file.parts.to.exclude  <- c("readPDF", "crossref", "Read", "zotero")
 exclude.patt <- paste0(file.parts.to.exclude, collapse = "|")
-filter.patt <- if (Sys.getenv("NOT_CRAN") == "true")
+filter.patt <- if (!identical(Sys.getenv("NOT_CRAN"), "true"))
                        paste0("^(?!", exclude.patt, ")[A-Za-z0-9]+$",
                             collapse = "|")
 
