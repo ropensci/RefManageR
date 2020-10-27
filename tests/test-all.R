d5c5e2fb111a1abf286783c77a0570cd08405564
Mathew W. McLean <mathew.w.mclean@gmail.com>
2020-10-27 21:11:48 +1100

    Update code for testthat tests on/off CRAN

--- tests/test-all.R
+++ tests/test-all.R
@@ -1,3 +1,10 @@
 library(testthat)
-## Sys.setenv(NOT_CRAN = "true")
-test_check("RefManageR", perl = TRUE, filter = "^(?!readPDF)[A-Za-z0-9]+$")
+Sys.setenv(NOT_CRAN = "true")
+## filter_patt <- "^(?!readPDF)[A-Za-z0-9]+$"
+file.parts.to.exclude  <- c("readPDF", "crossref", "Read", "zotero")
+exclude.patt <- paste0(file.parts.to.exclude, collapse = "|")
+filter.patt <- if (Sys.getenv("NOT_CRAN") == "true")
+                       paste0("^(?!", exclude.patt, ")[A-Za-z0-9]+$",
+                            collapse = "|")
+
+test_check("RefManageR", perl = TRUE, filter = filter.patt)
