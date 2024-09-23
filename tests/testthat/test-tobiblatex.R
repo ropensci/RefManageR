5ca98583834886cbcd1a90e43670dab9dafe4ed8
Mathew W. McLean <mathew.w.mclean@gmail.com>
2024-09-23 17:06:48 +1000

    Add tests for last commit

---
+++ tests/testthat/test-tobiblatex.R
@@ -0,0 +1,31 @@
+bib = BibEntry(bibtype = "article", 
+                 key = "shiotsuki2011kasai", 
+                 title = "葛西賢太著,『現代瞑想論-変性意識がひらく世界-』",
+                 author = "塩,亮子 and 葛西,賢太", 
+                 journal = "宗教と社会",
+                 volume = 17,
+                 pages = "67--69",
+                 year = 2011, 
+                 publisher = "「宗教と社会」学会")
+
+bib2 = BibEntry(bibtype = "article", 
+               key = "shiotsuki2011kasai", 
+               title = "Hello",
+               author = "Jane Doe", 
+               journal = "no",
+               volume = 17,
+               pages = "67--69",
+               year = 2011, 
+               publisher = "yes")
+
+test_that("toBiblatex doesn't replace CJK character name lists with ???? (#106)",
+{
+  out <- toBiblatex(bib)
+  expect_false(any(grepl("[?]", out)))
+})
+
+test_that("toBibtex doesn't replace CJK character name lists with ???? (#106)",
+{
+  out <- toBibtex(bib)
+  expect_false(any(grepl("[?]", out)))
+})
