4a7545850efa2b4d1e9a2457520ec51c1d9d2cdd
Mathew W. McLean <mathew.w.mclean@gmail.com>
2024-10-08 23:15:19 +1100

    More retries and sleep time for flaky test
    Signed-off-by: Mathew W. McLean <mathew.w.mclean@gmail.com>

--- tests/testthat/test-pubmed.R
+++ tests/testthat/test-pubmed.R
@@ -60,6 +60,6 @@ test_that("GetPubMedByID: Multiple books parsed correctly #86",
     
     ids <- c("33780208", "33764725")
     names(ids) <- c("geary2021variation", "brennan2021potential")
-    try_again(3, {Sys.sleep(2); bib <- GetPubMedByID(ids)})
+    try_again(5, {Sys.sleep(3); bib <- GetPubMedByID(ids)})
     expect_equal(unlist(bib$eprint), ids)
 })
