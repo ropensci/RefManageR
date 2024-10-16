1f07f05ab3d9adc40e5c5bc239f349b7d122a589
Mathew W. McLean <mathew.w.mclean@gmail.com>
2024-10-16 23:31:06 +1100

    Refactor GetPubMedByID tests
    * Attempt each GetPubMedByID test once with the entrez api,
    then use a cached/mock response if the request fails; add a
    failed test if all tests with API result in http errors
    
    Signed-off-by: Mathew W. McLean <mathew.w.mclean@gmail.com>

--- tests/testthat/test-pubmed.R
+++ tests/testthat/test-pubmed.R
@@ -5,38 +5,66 @@ context("PubMed")
 ## ReadGS
 ## ReadPubMed
 
-
-test_that("GetPubMedByID can process books (#2)", {
-  skip_if_offline("eutils.ncbi.nlm.nih.gov")
+TestPubMed <- function(name, ids, response, expect_code, warn = FALSE)
+{
+  error <- ""
   Sys.sleep(3)
-
-  test <- GetPubMedByID(c("24977996", "24921111"))
-  if (length(test))
-    expect_true(length(test) == 2L)
-})
-
-test_that("GetPubMedByID uses collective name if authors missing (#2)", {
-  skip_if_offline("eutils.ncbi.nlm.nih.gov")
-  Sys.sleep(3)
-
-  try_again(3, test <- GetPubMedByID(c(11678951, 15373863)))
-  if (length(test)){
-    authors <- unlist(test$author)
-    expect_true(length(authors) == 2L)
+  if (warn)
+  {
+    BibOptions(check.entries = FALSE)
+    expect_output <- expect_warning
+  }else{
+    BibOptions(check.entries = TRUE)
+    expect_output <- expect_silent
   }
-})
+  warnings <- capture_warning(bib <- try(GetPubMedByID(ids), TRUE))
+  test_that(name,
+  {
+    if (inherits(bib, "try-error"))
+    {
+      error <- attr(bib, "condition")$message
+      bib <- with_mocked_bindings(POST = function(...)
+        response,
+        expect_output(GetPubMedByID(ids)))
+    }else if (warn)
+      expect_length(warnings, 2L)
 
-test_that("GetPubMedByID warns if authors missing (#3)", {
-  skip_if_offline("eutils.ncbi.nlm.nih.gov")
-  Sys.sleep(3)
+    eval(expect_code)
+  })
+  return(error)
+}
 
-  BibOptions(check.entries = FALSE)
-  expect_warning(try_again(3, GetPubMedByID("7936917")))
-})
+responses <- readRDS("pubmed_responses.rds")
+test.names <- names(responses)
+ids <- setNames(list(c("24977996", "24921111"),
+                     c(11678951, 15373863),
+                     "7936917",
+                     "23891459",
+                     setNames(c("33780208", "33764725"),
+                              c("geary2021variation", "brennan2021potential"))),
+                test.names)
+warnings <- logical(length(responses))
+warnings[grepl("warn", names(responses), fixed = TRUE)] <- TRUE
 
+expect_code <- list(quote(expect_length(bib, 2L)),
+                   quote({
+                     authors <- unlist(bib$author)
+                     expect_true(length(authors) == 2L)
+                   }),
+                   quote(expect_length(bib, 1L)),
+                   quote({
+                     expect_equal(bib$year, "2013")
+                     expect_equal(bib$month, "07")
+                   }),
+                   quote(expect_equal(unlist(bib$eprint), ids)))
+errors <- Map(TestPubMed, test.names, ids, responses, expect_code, warnings)
+all.errored <- all(vapply(errors, nchar, 1L) > 0)
+expect_false(all.errored, label = paste(c("All GetPubMedbyID API requests failed",
+                                          errors), collapse = "\n"))
+ 
 test_that("LookupPubMedID successfully retrieves and add ID's'", {
     skip_if_offline("eutils.ncbi.nlm.nih.gov")
-    
+
     file.name <- system.file("Bib", "RJC.bib", package="RefManageR")
     bib <- ReadBib(file.name)
     Sys.sleep(3)
@@ -46,23 +74,4 @@ test_that("LookupPubMedID successfully retrieves and add ID's'", {
     expect_equal(ids, c("19381352", "19444335"))
     Sys.sleep(3)
     expect_message(try_again(3, LookupPubMedID(bib, 453)), "No PubMed ID's found")
-})
-
-test_that("GetPubMedByID reading of years/months (#52)", {
-  skip_if_offline("eutils.ncbi.nlm.nih.gov")
-  try_again(3, {Sys.sleep(3); bib <- GetPubMedByID("23891459")})
-  expect_equal(bib$year, "2013")
-  expect_equal(bib$month, "07")
-})
-
-test_that("GetPubMedByID: Multiple books parsed correctly #86",
-{
-    skip_if_offline("eutils.ncbi.nlm.nih.gov")
-
-    ids <- c("33780208", "33764725")
-    names(ids) <- c("geary2021variation", "brennan2021potential")
-    bib <- with_mocked_bindings(POST = function(...)
-        readRDS("pubmedid_response.rds"),
-                GetPubMedByID(ids))
-    expect_equal(unlist(bib$eprint), ids)
 })
